9ced4b415bcde23d8e02e5f4c3cd96351a517b00
Vlady <vladyslav.v.zasoba@mercedes-benz.com>
2023-12-10 19:53:59 +0100

    47110522: Implement design system
    - use main colors
    - Add SASS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {HomePage} from "./pages/home/HomePage.tsx";
 import {ErrorPage} from "./pages/error/ErrorPage.tsx";
